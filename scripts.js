f5d9e173c5872b86d627859f8734ce98019ca6da
jodjow <jsdiass96@gmail.com>
2025-07-14 16:42:51 -0300

    calculando total de despesas

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const category = document.getElementById("category")
 //seleciona os elementos da lista
 const expenseList = document.querySelector("ul")
 const expensesQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
 
 
 //capturando evento input para formatar valor
@@ -109,6 +110,39 @@ function updateTotals(){
         
         //atualiza a quantidades de itens da lista
         expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+        //variavel para incrementar o total
+        let total = 0
+        
+        //percorre cada item da lista 
+        for(let item = 0; item < items.length; item++){
+            const itemAmount = items[item].querySelector(".expense-amount")
+            
+            //substitui caracteres nao numericos e substitui a virgula pelo ponto
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",",".")
+            //converte o valor para float
+            value = parseFloat(value)
+            //verifica se é um numero valido
+            if(isNaN(value)){
+                return("Não foi possível calcular o total, o valor não parece um número.")
+            }
+            //incrementa o valor total
+            total += Number(value)
+        }
+
+      //cria span para add R$ formatado
+      const symbolBRL = document.createElement("small")
+      symbolBRL.textContent = "R$"
+
+      //formata o valor e remove o R$ que sera exibido pela small com um estilo customizado
+      total = formatCurrencyBRL(total).toUpperCase().replace("R$","")
+      
+      //limpa o conteudo do elemento
+      expensesTotal.innerHTML = ""
+    
+      //add o simbolo da moeda e o valor total formatado
+      expensesTotal.append(symbolBRL, total)
+
     }catch(error) {
         console.log(error)
         alert("Não foi possível atualizar os totais.")
